9b4124cdcfa21d2f7d3b9d87e01a02b840e85a2e
hatsy <hadrien.renold@gmail.com>
2016-04-08 11:17:05 -0700

    Migrated connection to Google Drive API to a new local branch

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,5 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
-
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
-APP_STATIC = os.path.join(APP_ROOT, 'static')
 
 from app import views, models
