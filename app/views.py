9ac191bace37b3c6de879b11bdbcdb3c7eafa6ef
hatsy <hadrien.renold@gmail.com>
2016-04-09 20:10:26 -0700

    File picker and preview v0.1

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, request
 from app import app, models
 
+test_file_url = 'https://www.filestackapi.com/api/file/S8O2rWoSFewERT8AfNxW'
+
 @app.route('/')
 def index():
     return redirect('/main')
@@ -12,6 +14,19 @@ def main():
 @app.route("/createUpload")
 def createUpload():
     return render_template("createUpload.html")
+
+@app.route("/previewUpload", methods=['POST'])
+def previewUpload():
+    global test_file_url
+    return render_template("previewUpload.html", url=test_file_url)
+
+@app.route("/saveFile", methods=["POST"])
+def saveFile():
+    print("Saved file")
+    global test_file_url
+    test_file_url = "https://www.filestackapi.com/api/file/" + request.form["url"].split('/')[-1]
+    print(test_file_url)
+    return "Saved file"
 
 @app.route("/createSearch")
 def createSearch():
