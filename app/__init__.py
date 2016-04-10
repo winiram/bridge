1e1c015ae7aaba9e3a809b2369b7aa7e7260c87d
Daniel W. Chen <daniel@ischool.berkeley.edu>
2016-04-09 23:09:06 -0700

    Removed login related parts

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,8 @@
-from flask.ext.login import LoginManager
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-lm = LoginManager()
-lm.init_app(app)
 
 from app import views, models
--- app/models.py
+++ app/models.py
@@ -8,35 +8,5 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=False)
     password = db.Column(db.String(20), unique=False)
 
-    #In general this method should just return True unless the object represents a user that should not be
-    #allowed to authenticate for some reason.
-    @property
-    def is_authenticated(self):
-        return True
-
-    #The is_active property should return True for users unless they are inactive,
-    #for example because they have been banned.
-    @property
-    def is_active(self):
-        return True
-
-    #should return True only for fake users that are not supposed to log in to the system.
-    @property
-    def is_anonymous(self):
-        return False
-
-    #should return a unique identifier for the user, in unicode format.
-    def get_id(self):
-        try:
-            return unicode(self.id)  # python 2
-        except NameError:
-            return str(self.id)  # python 3
-
-    #loads a user from the database.
-    @lm.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
-
     def __repr__(self):
         return '<Customer %r>' % self.id
