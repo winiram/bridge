2b213c968feef5bdb6c523f5a14a78a0459b733a
gracenbrilz <gracenbrilmyer@ischool.berkeley.edu>
2016-04-18 16:35:22 -0700

    added templates for viz and search interface, connected to databaseCache.json

--- app/views.py
+++ app/views.py
@@ -21,6 +21,15 @@ def createSearch():
 def previewSearch():
     return render_template("previewSearch.html")
 
-@app.route("/searchInterface")
+@app.route("/interface")
 def searchInterface():
-    return render_template("searchInterface.html")
+    return render_template("interface.html")
+
+@app.route("/search")
+def searchInterface_side():
+    return render_template("search.html")        
+
+
+@app.route("/test")
+def test():
+    return render_template("test.html")   
