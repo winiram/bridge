8ace31bcbdf069903616c225cf7ae73980771d2b
Daniel W. Chen <daniel@ischool.berkeley.edu>
2016-04-21 14:16:12 -0700

    database working with createupload and createsearchsimple(limited to one row)

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, IntegerField, PasswordField
+from wtforms import StringField, IntegerField, PasswordField, FieldList, SelectField
 from flask_wtf.html5 import EmailField
 from wtforms.validators import DataRequired, Email
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
@@ -19,4 +19,16 @@ class SignupForm(Form):
         Unique(User, User.email,message='There is already an account with that email.')])
     password = PasswordField('password', validators=[DataRequired()])
 
-#class SearchInterfaceForm(Form):
+# choices = [('test', 'Test'), ('test2', 'Test')]
+display_choices = [('textbox', 'Textbox'), ('dropdown', 'Dropdown')]
+
+class SearchInterfaceForm(Form):
+    fieldname = StringField('fieldname', validators=[DataRequired()])
+    description = StringField('description', validators=[DataRequired()])
+    # header = SelectField(label='header', choices=choices)
+    header =SelectField(label='header')
+    display = SelectField(label='display', choices=display_choices)
+
+    def __init__(self, header):
+        super(SearchInterfaceForm, self).__init__()
+        self.header.choices = header
