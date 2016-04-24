6df2c5c9eefd4f75d5605335b15a83dbffa2537e
gracenbrilz <gracenbrilmyer@ischool.berkeley.edu>
2016-04-24 10:48:40 -0700

    updated css, and adding buttons to create record detils pop-up

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -10,12 +10,17 @@
   },
   success: function(data){
     console.log("succeess")
-    // console.log(data.length)
-
 var table = [];
 var headers = [];
 var headerRow=[];
 var gridView = [];
+var rowNum =0;
+
+ // <button data-target="m0" class="btn modal-trigger">Modal</button>
+
+var deetButtonStart =  '<td> <a class="waves-effect waves-light btn modal-trigger" href="#'
+var deetButtonEnd = '">Q</a></td>'
+
 
     for(i in data){
 
@@ -31,39 +36,57 @@ var gridView = [];
         }
 
  // make rows (list and grid views)
+  var count2 = []
         for(j in val){
+
 // get values
             var table_header = j;
             var sub_val = val[j];
         
             if(table_header == "imglink"){
-              console.log("img!!")
+              var gridRow = ' <img src="'+ sub_val + '">'
+              gridItem += gridRow;
               continue}
 // make headers, put the id as the header name
             headers.push("<th data-field='"+table_header+"'>"+ table_header+"</th>");             
-// make rows for list view
+// make rows for list view and restrict the collumns
+           count2.push('x')
+           if(count2.length <= 5){
+
             var record = '<td>' + sub_val + '</td>'   
             row.push(record);
+          
 // make grid items
-            var gridRow = '<p>'+ table_header + ": " + sub_val + '</p>'
+            var gridRow = '<p><b>'+ table_header + ":</b> " + sub_val + '</p>'
             gridItem += gridRow;   
+            };
         }
-         table.push("<tr>"+row+"</tr>")
+// adds table rows with details button (and row number as ID)
+         table.push('<tr>'+ deetButtonStart + rowNum + deetButtonEnd + row +"</tr>")
          gridView.push("<div class= 'col s3 gridItem'>"+ gridItem +"</div>")
-         
-      }
-   // make headers
+         // console.log(rowNum)
+         rowNum+=1;
 
-            
-      for (i = 0; i < count.length-1; i++) { 
-
+    };
+    headerRow.push("<th></th>")
+   // make headers and restrict their legnth too
+      for (i = 0; i < 5; i++) { 
 
         headerRow.push(headers[i])
       }
+      
 
 $("#table_head").append('<tr>'+ headerRow+'/<tr>')
 $("#table_results").append(table)
 $("#gridView").append(gridView)
+// $("#0").click(function(){
+//   alert("hiii");
+// });
+
+
+
+
+
 
 }
 });
@@ -71,23 +94,58 @@ $("#gridView").append(gridView)
 /////////////////////////////////// END POPULATE HEADERS AND RECORD DATA /////////////////////////////////
 
 
-$(document).ready(function() {
-    $('<rect>').click(function(e) {  
-      alert(1);
-    });
-});
+function deets(q){
+  // console.log(q.id)
+
+  $.ajax({
+  url: '../static/databaseCache.json',
+  type: 'get',
+  dataType: 'json',
+  error: function(data){
+    console.log("an error getting the record has occured")
+    
+  },
+  success: function(data){
+    console.log("got the data")
+
+
+  for(i in data){
+        var key = q.id;
+        if(Number(i)===Number(key)){
+          console.log(key)
+        var val = data[i];
+        console.log(val)
+
+
+
+
+        // for(j in val){
+
+        // // get values
+        //             var table_header = j;
+        //             var sub_val = val[j];
+                
+        //             if(table_header == "imglink"){
+        //               var gridRow = ' <img src="'+ sub_val + '">'
+        //               gridItem += gridRow;
+        //               continue}
+
+
+
+
+
+        }
+      }
+    }
+  }
+  )};
+
 
 /////////////////////////// SEARCH FIELDS ////////////////////////////////////////////////////////
 $(function() {
-      $("#clearbtn").click( function()
+      $(".clearbtn").click( function()
            { console.log("clear button is clicked!")
-
-           // $( ".searchItem" ).each(function( index ) {
-           //   var queryStr = $(this).val();
- 
-           //    console.log(queryStr);
-
-           //  });
+           $(this).closest('form').find("input[type=text], textarea").val("");
 
 
            }
@@ -95,7 +153,7 @@ $(function() {
 });
 
 $(function() {
-      $("#searchbtn").click( function()
+      $(".searchbtn").click( function()
            { console.log("search button is clicked!")
            // gets all form data
            $( ".searchItem" ).each(function( index ) {
